c51c71cffef49665d2c3d8b28e6af026a08bb410
Ionică Bizău <bizauionica@gmail.com>
2016-09-05 12:48:10 +0300

    Added files

---
+++ build.sh
@@ -0,0 +1,11 @@
+echo "Browserifying..."
+browserify --node -s BloggifyServer lib/index.js -o dist/bloggify.js --exclude lien
+
+echo "Replacing octals."
+sed -i -- 's/ 0777 / 0o777 /g' dist/bloggify.js
+
+echo "Babelfiying."
+babel dist/bloggify.js -o dist/bloggify.js
+
+echo "Uglifying..."
+uglifyjs dist/bloggify.js -c -m -o dist/bloggify.js
