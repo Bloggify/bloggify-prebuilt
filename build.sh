3df97f169585c9823899a1f0f1e174b04231fea3
Ionică Bizău <bizauionica@gmail.com>
2016-11-20 15:57:23 +0200

    :arrow_up: 1.0.15 :tada:

--- build.sh
+++ build.sh
@@ -2,16 +2,16 @@ rm -rf node_modules
 npm i
 
 echo "Browserifying..."
-browserify --node -s BloggifyServer lib/index.js -o dist/bloggify.js --exclude lien
+./node_modules/.bin/browserify --node -s BloggifyServer lib/index.js -o dist/bloggify.js --exclude lien
 
 echo "Replacing octals."
 sed -i -- 's/ 0777 / 0o777 /g' dist/bloggify.js
 
 echo "Babelfiying."
-#babel dist/bloggify.js -o dist/bloggify.js
+./node_modules/.bin/babel dist/bloggify.js -o dist/bloggify.js
 
 echo "Uglifying..."
-#uglifyjs dist/bloggify.js -c -m -o dist/bloggify.js
+./node_modules/.bin/uglifyjs dist/bloggify.js -c -m -o dist/bloggify.js
 
 ship-release bump
 ship-release publish -d 'Rebuild.'
