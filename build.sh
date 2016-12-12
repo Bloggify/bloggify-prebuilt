6ffaa4695322390244f7bdfcafe28260a22473ec
Ionică Bizău <bizauionica@gmail.com>
2016-12-12 15:49:21 +0200

    :arrow_up: 1.0.22 :tada:

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
-rm -rf node_modules
-npm i
+#rm -rf node_modules
+#npm i
 
 echo "Browserifying..."
-./node_modules/.bin/browserify --node -s BloggifyServer lib/index.js -o dist/bloggify.js --exclude lien --exclude sugarss --exclude pkg-resolve --exclude fsevents
+./node_modules/.bin/browserify --node -s BloggifyServer lib/index.js -o dist/bloggify.js --exclude lien --exclude rucksack
 
 echo "Babelfiying."
 ./node_modules/.bin/babel dist/bloggify.js -o dist/bloggify.js
